342c1a7659ec39502846810ef5be429727e7eb0e
Bill Long <bilong@microsoft.com>
2018-06-06 02:36:42 -0500

    Switch to IndexedDB for storage
    We cannot use NeDB as we first intended, because it does not allow
    multiple processes to access the database at the same time. And since
    we're using Electron, each window will be a process.
    
    However, this does work with IndexedDB, so long as we use
    makeSingleInstance.

--- Electron/src/app/providers/database.service.ts
+++ Electron/src/app/providers/database.service.ts
@@ -1,27 +1,119 @@
 import { Injectable } from '@angular/core';
 import { ElectronService } from './electron.service';
 
+const DATABASE_NAME = 'messagesDb';
+const OBJECTSTORE_NAME = 'messages';
+const RAWID_INDEX = 'RawId, ProviderName';
+const SHORTID_INDEX = 'ShortId, ProviderName';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
 
-  messageDatabaseFilename = 'Messages.db';
-  db: Nedb;
+  db: IDBDatabase;
 
   constructor(private electronService: ElectronService) {
-    this.db = new electronService.nedb({ filename: this.messageDatabaseFilename, autoload: true });
+    const openReq = indexedDB.open(DATABASE_NAME, 1);
+    openReq.onerror = (ev) => {
+      console.log(`Failed to open ${DATABASE_NAME}`, ev);
+    };
+
+    openReq.onsuccess = (ev: any) => {
+      this.db = ev.target.result;
+    };
+
+    openReq.onupgradeneeded = (ev: any) => {
+      this.db = ev.target.result;
+      let objectStore;
+      if (ev.oldVersion < 1) {
+        objectStore = this.db.createObjectStore(OBJECTSTORE_NAME, { autoIncrement: true });
+      } else {
+        objectStore = ev.target.transaction.objectStore(OBJECTSTORE_NAME);
+      }
+
+      objectStore.createIndex(RAWID_INDEX, ['RawId', 'ProviderName'], { unique: false });
+      objectStore.createIndex(SHORTID_INDEX, ['ShortId', 'ProviderName'], { unique: false });
+    };
   }
 
-  addMessages(messages: any) {
-    this.db.insert(messages);
+  addMessages(messages: any[]) {
+    return new Promise<number>(resolve => {
+      let itemsAdded = 0;
+      const transaction = this.db.transaction(OBJECTSTORE_NAME, 'readwrite');
+      transaction.oncomplete = ev => resolve(itemsAdded);
+      transaction.onerror = ev => resolve(itemsAdded);
+      const messageStore = transaction.objectStore(OBJECTSTORE_NAME);
+      messages.forEach(m => {
+        const req = messageStore.add(m);
+        itemsAdded++;
+      });
+    });
   }
 
   getAllMessages() {
-    return this.db.getAllData();
+    return new Promise<any[]>(resolve => {
+      const messages = [];
+      this.db.transaction(OBJECTSTORE_NAME).objectStore(OBJECTSTORE_NAME).openCursor().onsuccess = (ev: any) => {
+        const cursor = ev.target.result;
+        if (cursor) {
+          messages.push(cursor.value);
+          cursor.continue();
+        } else {
+          resolve(messages);
+        }
+      };
+    });
   }
 
-  async getMessages(provider: string, rawId: number) {
-    return await this.db.find({ ProviderName: provider, RawId: rawId });
+  deleteAllMessages() {
+    return new Promise(resolve => {
+      this.db.transaction(OBJECTSTORE_NAME, 'readwrite')
+        .objectStore(OBJECTSTORE_NAME)
+        .clear()
+        .onsuccess = (ev) => resolve();
+    });
+  }
+
+  /**
+   * Search for an event by RawID. If we do not find an
+   * event with the same RawID, we look for one with a
+   * ShortID that matches the provided RawID.
+   * @param providerName The name of the event provider
+   * @param id The raw ID of event.
+   */
+  findMessages(providerName: string, id: number) {
+    return this.getMessages(providerName, id, true);
+  }
+
+  /**
+   * Private method to do the actual work of searching, first
+   * on raw ID and then on short ID.
+   * @param providerName The name of the event provider
+   * @param id The RawID of the event
+   * @param useRawId Whether we should try to match on RawId or ShortId
+   */
+  private getMessages(providerName: string, id: number, useRawId: boolean) {
+    return new Promise<any[]>(resolve => {
+      const range = IDBKeyRange.only([id, providerName]);
+      const results = [];
+      const index = this.db.transaction(OBJECTSTORE_NAME)
+        .objectStore(OBJECTSTORE_NAME)
+        .index(useRawId ? RAWID_INDEX : SHORTID_INDEX)
+        .openCursor(range)
+        .onsuccess = (ev: any) => {
+          const cursor = ev.target.result;
+          if (cursor) {
+            results.push(cursor.value);
+            cursor.continue();
+          } else {
+            if (results.length < 1 && useRawId) {
+              this.getMessages(providerName, id, false).then(shortIdResult => resolve(shortIdResult));
+            } else {
+              resolve(results);
+            }
+          }
+        };
+    });
   }
 }
